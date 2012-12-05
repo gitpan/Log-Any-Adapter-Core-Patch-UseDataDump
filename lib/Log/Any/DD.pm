7f7f1977b573a05413dfefa53b728f2280b09a8f
Steven Haryanto <stevenharyanto@gmail.com>
2012-12-05 10:58:29 +0000

    Import of SHARYANTO/Log-Any-Adapter-Core-Patch-UseDataDump-0.01 from CPAN.
    gitpan-cpan-distribution: Log-Any-Adapter-Core-Patch-UseDataDump
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SHARYANTO/Log-Any-Adapter-Core-Patch-UseDataDump-0.01.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

---
+++ lib/Log/Any/DD.pm
@@ -0,0 +1,30 @@
+package Log::Any::DD;
+use Log::Any::Adapter::Core::Patch::UseDataDump;
+our $VERSION = '0.01'; # VERSION
+1;
+# ABSTRACT: Shortcut for Log::Any::Adapter::Core::Patch::UseDataDump
+
+__END__
+=pod
+
+=head1 NAME
+
+Log::Any::DD - Shortcut for Log::Any::Adapter::Core::Patch::UseDataDump
+
+=head1 VERSION
+
+version 0.01
+
+=head1 AUTHOR
+
+Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
